439051eb889f75135c34ce410ab8db8c83b3e144
Phan Dai <phandai2112@gmail.com>
2021-04-12 18:45:29 +0700

    bt

---
+++ BT/BT.js
@@ -0,0 +1,26 @@
+//BT1
+var Gr1=12;
+var Gr2;
+for(var i=0;i<=4;i++){
+	if(i==4){
+		Gr2=2*Gr1-i;
+		document.write(Gr2);
+		document.write("<br>");	}
+}
+//BT3
+var Bag1 = 18;
+var Bag2 = Bag1 * 2;
+var Ball=0;
+while(Bag2 -- > Bag1++){
+	Ball++;
+}
+document.write(Ball);
+//BT4
+// var Book1=27;
+// var Book2;
+// var Many=0;
+// Book1=3*Book2;
+// do{
+	
+// }
+// while(Book2*2=Book1);
